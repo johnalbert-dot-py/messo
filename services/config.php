806ef9b99c5181073c393bcb0fd3568223e5c030
John Albert Flores <floresjohnalbert19@gmail.com>
2022-07-23 17:59:42 +0800

    updated config

--- services/config.php
+++ services/config.php
@@ -11,16 +11,16 @@ class ERROR_CODE
 
 class LinkedInConfig
 {
-    public static $CLIENT_ID = '866y3tf8mik0gm';
-    public static $CLIENT_SECRET = '2HAxXgsGTO4TE66L';
+    public static $CLIENT_ID = '';
+    public static $CLIENT_SECRET = '';
     public static $REDIRECT_URI = 'http://localhost/views/';
 }
 
 class MicrosoftConfig
 {
-    public static $APP_ID = "f6718016-e0ca-477e-bb95-cbc10408fc44";
-    public static $TENNANT_ID = "14ef7792-dfb2-45cb-9077-165f359154ad";
-    public static $SECRET_KEY = "e660612a-728f-4b78-8521-de9de16ce9cf";
+    public static $APP_ID = "";
+    public static $TENNANT_ID = "";
+    public static $SECRET_KEY = "";
     public static $REDIRECT_URI = "http://localhost/views/";
 
     public static function getLoginUrl($redirect_uri = "")
@@ -39,15 +39,15 @@ class MicrosoftConfig
 
 class GoogleConfig
 {
-    public static $CLIENT_ID = '368377028020-isg5m4srca4kshl91le3i6vebh27cac6.apps.googleusercontent.com';
-    public static $CLIENT_SECRET = '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';
+    public static $CLIENT_ID = '';
+    public static $CLIENT_SECRET = '';
     public static $REDIRECT_URI = "http://localhost/views/";
 }
 
 
 class FacebookConfig
 {
-    public static $APP_ID = "583922600014772";
-    public static $APP_SECRET = "90c916d377ef8f243d336aec88d90cf1";
+    public static $APP_ID = "";
+    public static $APP_SECRET = "";
     public static $REDIRECT_URI = "http://localhost/views/";
 }
